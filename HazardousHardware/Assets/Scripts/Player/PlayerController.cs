67088c4b7c9915eda504294fa15a46feae5e3950
Jim-P-enis <90990701+JIMthaGR8@users.noreply.github.com>
2024-03-05 10:09:16 -0500

    skyboxes added
    added skyboxes to asset list

--- HazardousHardware/Assets/Scripts/Player/PlayerController.cs
+++ HazardousHardware/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        GameManager.Instance.TestGameManager();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float fInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveInput = new Vector3(hInput, rb.velocity.y, fInput ).normalized;
         
+        rb.velocity = moveInput * speed;
+
+        
+
     }
 }
