611ec73c222a04b5f49a417320c72d5c3dc99f64
Jim-P-enis <90990701+JIMthaGR8@users.noreply.github.com>
2024-03-07 08:34:11 -0500

    mroe scene changes
    more changes to scene

--- HazardousHardware/Assets/Scripts/Player/PlayerController.cs
+++ HazardousHardware/Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -12,8 +13,28 @@ public class PlayerController : MonoBehaviour
 
 
     CharacterController cc;
+
+    public float YVelocity;
     void Start()
     {
+        try
+        {
+            cc = GetComponent<CharacterController>();
+
+            if(speed < 0)
+            {
+                speed = 20;
+                throw new ArgumentException("default value has been set for speed");
+            }
+        }
+        catch ( NullReferenceException e)
+        {
+            //
+        }
+        catch(ArgumentException e)
+        {
+            //
+        }
         cc = GetComponent<CharacterController>();
         GameManager.Instance.TestGameManager();
     }
@@ -21,16 +42,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //YVelocity = cc.velocity.y;
         float hInput = Input.GetAxisRaw("Horizontal");
         float fInput = Input.GetAxisRaw("Vertical");
 
         Vector3 moveInput = new Vector3(hInput, 0, fInput ).normalized;
         
-        moveInput *= speed;
-        moveInput.y -= gravity;
+        moveInput *= speed * Time.deltaTime;
+        //moveInput.y -= gravity;
 
-        moveInput *= Time.deltaTime;
+        YVelocity = (!cc.isGrounded) ? YVelocity -= gravity * Time.deltaTime : 0;
 
+        if (cc.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            //moveInput.y = jumpSpeed;
+            YVelocity = jumpSpeed;
+        }
+
+
+        moveInput.y = YVelocity;
         cc.Move(moveInput);
        // rb.velocity = new Vector3(moveInput.x * speed, rb.velocity.y, moveInput.z * speed);
 
